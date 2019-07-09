232c0e1bef8fc4def9c5c2fb593ca47891886a3f
Matt Werner <mrwerner392@gmail.com>
2019-07-09 21:05:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,7 +11,7 @@ def find_max_value(array)
   max_value = 0
   array.length.times do |index|
     if array[index] > max_value
-      max_value = array.index
+      max_value = array[index]
     end
   end
   max_value
